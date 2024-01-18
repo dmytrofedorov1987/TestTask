b2bef5ead39dbdb8c626aae98739c234b35035d8
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2024-01-18 13:14:04 +0200

    22222

--- src/main/java/com/example/testtask/AddWarehouseServlet.java
+++ src/main/java/com/example/testtask/AddWarehouseServlet.java
@@ -1,7 +1,6 @@
 package com.example.testtask;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * Need connection and create database from checking.
@@ -21,17 +19,18 @@ public class AddWarehouseServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        /* resp.setContentType("application/json");
-       warehouseDAOImp.createTable();
-        String jsonWarehouse = req.getParameter("warehouse");
+        // resp.setContentType("application/json");
+        WarehouseDAOImp warehouseDAOImp = new WarehouseDAOImp();
+        warehouseDAOImp.createTable();
+        String jsonWarehouse = req.getParameter("Warehouse");
         ObjectMapper objectMapper = new ObjectMapper();
         Warehouse warehouse = objectMapper.readValue(jsonWarehouse, Warehouse.class);
         warehouseDAOImp.addWarehouse(warehouse);
 
-         */
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Success");
-        printWriter.close();
 
+        //resp.setContentType("text/html");
+        //PrintWriter printWriter = resp.getWriter();
+        //printWriter.write("add!");
+        //printWriter.close();
     }
 }
--- src/main/java/com/example/testtask/GetWarehouseServlet.java
+++ src/main/java/com/example/testtask/GetWarehouseServlet.java
@@ -1,8 +1,33 @@
 package com.example.testtask;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/get")
 public class GetWarehouseServlet extends HttpServlet {
+    /*
+        private final WarehouseDAOImp warehouseDAOImp;
+        public GetWarehouseServlet(WarehouseDAOImp warehouseDAOImp) {
+            this.warehouseDAOImp = warehouseDAOImp;
+        }
+    */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //warehouseDAOImp.createTable();
+        WarehouseDAOImp warehouseDAOImp = new WarehouseDAOImp();
+        //Warehouse warehouse = new Warehouse(1,"fff", "ffdfd","eee", "ttt", "jjhjhj", " d ", 111);
+        Warehouse warehouse = warehouseDAOImp.getWarehouse(1);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(warehouse);
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(json);
+    }
 }
